9e5b1bb6637eee8eb612d629af081fee24f8b537
theJian <thejianmail@gmail.com>
2015-12-06 20:49:18 +0800

    add login strategy

--- config/passport.js
+++ config/passport.js
@@ -14,18 +14,22 @@ module.exports = function (passport) {
 		});
 	});
 
+	// local signup
 	passport.use('local-signup', new LocalStrategy({
-		usernameField: 'email',
-		passwordField: 'password',
+		usernameField: 'Email',
+		passwordField: 'Password',
 		passReqToCallback: true
-	}), function (req, email, password, done) {
+	}, function (req, email, password, done) {
 		
 		process.nextTick(function () {
 			User.findOne({'local.email' : email }, function (err, user) {
 				if(err) return done(err);
+
+				// if user exists
 				if(user) {
 					return done(null, false, req.flash('signupMessage', 'Email is already taken'));
 				} else {
+					//user doesn't exist, create a new user
 					var newUser = new User();
 					newUser.local.email = email;
 					newUser.local.password = newUser.generateHash(password);
@@ -39,5 +43,30 @@ module.exports = function (passport) {
 				}
 			});
 		});
-	});
+	}));
+
+	// local login
+	passport.use('local-login', new LocalStrategy({
+		usernameField: 'Email',
+		passwordField: 'Password',
+		passReqToCallback: true
+	}, function (req, email, password, done) {
+		console.log('******************************');
+		User.findOne({'local.email' : email }, function (err, user) {
+			if(err) return done(err);
+
+			// user doesn't exist
+			if(!user) {
+				return done(null, false, req.flash('loginMessage', 'Wrong email or password'));
+			}
+
+			// wrong password
+			if(!user.isValidPassword(password)) {
+				return done(null, false, req.flash('loginMessage', 'Wrong email or password'));
+			}
+
+			// success login
+			return done(null, user);
+		});
+	}));
 }
