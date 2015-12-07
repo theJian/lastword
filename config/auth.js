919f40633e64f25d55743a807c573929d7be9fcb
theJian <thejianmail@gmail.com>
2015-12-07 22:47:47 +0800

    add auth.js

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+	'facebookAuth': {
+		clientID: '145389615823863',
+		clientSecret: '9c55b4c5cf7e17dcecabe49fdf0746ed',
+		callbackURL: 'http://localhost:3000/auth/facebook/callback',
+		enableProof: false
+	}
+};
