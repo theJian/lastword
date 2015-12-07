2a601bd727a1f581d4686a60398096a40233c484
呈秀波 <wutianzhe123@gmail.com>
2015-12-07 15:19:33 +0000

    express

--- node/verifier/verifier.js
+++ node/verifier/verifier.js
@@ -1,4 +1,18 @@
 // day 12.07.2015 by zhe13
 // an email-verifier with node.
 //  
-// IM 
+// A Verify process should meet these requirements following:
+// 1.time limitatiom:[5min-10min]
+// 2.frequence limitation
+// 3.IP limitted visitation 
+// 4.cross-domain principle
+// 5.
+
+// DATABASE DESIGN.
+// 
+
+
+var ip="127.0.0.1",
+now = Date.now();
+
+
