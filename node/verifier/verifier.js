3d61cf5bd82b84f2b1a00065bd04d177fa17c6ec
呈秀波 <wutianzhe123@gmail.com>
2015-12-09 10:37:55 +0000

    email validate

--- node/verifier/verifier.js
+++ node/verifier/verifier.js
@@ -1,6 +1,8 @@
 // day 12.07.2015 by zhe13
-// an email-verifier with node.
-//  
+//      an email-verifier with node.
+// day 12.08.2015 by zhe13
+//      require a verify module.
+  
 // A Verify process should meet these requirements following:
 // 1.time limitatiom:[5min-10min]
 // 2.frequence limitation
@@ -11,6 +13,7 @@
 // DATABASE DESIGN.
 // 
 
+require("./express.js")
 
 var ip="127.0.0.1",
 now = Date.now();
---
+++ node/verifier/verify3rd.js
@@ -0,0 +1,11 @@
+// day 12.9.2015 @zhe13
+//      use 3rd plugin emial-verify
+//      here are different modules,try to find one best.
+
+
+// email-validator @1.0.3 by sembiance
+var validator = require("email-validator");
+validator.validate("wutianzhe1314@wtz.net");
+// it failed.opps and go on with the next one
+
+// email-verification @0.3.1
