9da3e1838f7db8741aee306379e27814a73c140e
呈秀波 <lovewutianzhe@cuc.edu.cn>
2015-12-10 11:29:36 +0000

    meet problem with ejs.need time to read it

--- node/chatting/multter_server.js
+++ node/chatting/multter_server.js
@@ -3,9 +3,12 @@
 //      and it helps me to understand the server.js and router.js
 // day 12.10.2015 @zhe13
 //      transport from express 3.x to 4.x
+// day 12.10.2015 @zhe13
+//      add render.a function to redirect browsers
+
 
 // note :I DONT THINK ITs a good choic
-
+"use strict"
 var express = require("express");
 var body_parser = require("body-parser");
 var application = express();
@@ -15,14 +18,40 @@ application.listen(process.env.PORT,process.env.IP);
 var msgs = new Array();
 
 application.get('/',function(req,res){
-    res.send("Welcome to multter,User")
+    // res.send("Welcome to multter,User")
+    var title = "Multter";
+    var header= "Welcome to Multter";
+    
+    
+    res.render("index",{
+        "title":title,
+        "header":header,
+        "msgs":msgs,
+        stylesheets :[]
+    },function(err,html){
+        if(err){
+            console.log("err",err);
+            return;
+        }else{
+            console.log(html);
+        }
+    });
+    
 });
 
 // express no longer provides so many middleware,need to install it separately.
 application.post('/send',body_parser.urlencoded({extended:true}),function(req,res){
     if(req.body && req.body.msg){
         msgs.push(req.body.msg);
-        res.send({status:"OK",message:"received successfully"});
+        
+        
+        if(acceptsHtml(req.header["accept"])){
+            res.redirect('/',302);
+        }else{
+        res.send({status:"OK",message:"received successfully"});    
+        }
+        
+        
     }else{
         console.log(req);
         res.send({status:"WARNING",message:"received failed"});
@@ -33,3 +62,14 @@ application.get('/multter',function(req, res) {
     res.send(msgs);
 })
 
+
+// this function tells API clients from Browsers.
+function acceptsHtml(header){
+    var accepts = header.split(',');
+    for(let x in accepts){
+        if(accepts[x] === "text/html"){
+            return true;
+        }
+        return false;
+    }
+}
